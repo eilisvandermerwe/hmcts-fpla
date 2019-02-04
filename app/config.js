e6be752d6a6117c444d10f0948a423d10236391b
Eilis van der Merwe <eilis@vandermerwe.co.za>
2019-02-04 15:58:42 +0000

    initiating case list page

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Family and Public Law and Adoption',
 
   // Default port that prototype runs on
   port: '3000',
