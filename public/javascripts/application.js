ec71752ce36a7684fd313c9078e3aac3aefd2e63
Eilis van der Merwe <Eilis.vandermerwe@hmcts.net>
2020-07-24 11:28:56 +0100

    document upload for LA

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -31,6 +31,26 @@ function show2(){
   document.getElementById('div1').style.display = 'block';
 }
 
+function myFunction() {
+  var checkBox = document.getElementById("myCheck");
+  var text = document.getElementById("text");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
+
+function myFunction2() {
+  var checkBox = document.getElementById("myCheck2");
+  var text = document.getElementById("text2");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
+
 
 
 // $("a.govuk-back-link").click(function() {
